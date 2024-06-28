3bec2cdfdf488ff21758c00aacc0e2761e3a2255
Joe Leong <joeleong06@gmail.com>
2024-06-28 23:45:37 +0800

    mediapipe hand gesture detection with muti hand landmarks drawn

--- mediapipe_gesture_detect_test.py
+++ mediapipe_gesture_detect_test.py
@@ -15,7 +15,8 @@ camera = cv2.VideoCapture(0)
     
 options = gesture_recognizer_options(
     base_options = base_options(model_asset_path = "./models/gesture_recognizer.task"),
-    running_mode = vision_running_mode.IMAGE
+    running_mode = vision_running_mode.IMAGE,
+    num_hands=10
 )
 
 with gesture_recognizer.create_from_options(options) as recognizer:
@@ -23,22 +24,24 @@ with gesture_recognizer.create_from_options(options) as recognizer:
         sucess, frame = camera.read()
         mp_image = mp.Image(image_format = mp.ImageFormat.SRGB, data=frame)
         result = recognizer.recognize(mp_image)
+        print(len(result.hand_landmarks))
         if len(result.hand_landmarks) == 0:
             cv2.imshow("frame", frame)
             if cv2.waitKey(1) in [ord("q"), 27, 32]:
                 break
             continue
         
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()        
-        hand_landmarks_proto.landmark.extend([landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in result.hand_landmarks[0]])
         
+        for i in range(len(result.hand_landmarks)):
+            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()        
+            hand_landmarks_proto.landmark.extend([landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in result.hand_landmarks[i]])
         
-        mp_drawing.draw_landmarks(
-            frame,
-            hand_landmarks_proto,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
+            mp_drawing.draw_landmarks(
+                frame,
+                hand_landmarks_proto,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
         
         cv2.imshow("frame", frame)
         if cv2.waitKey(1) in [ord("q"), 27, 32]:
